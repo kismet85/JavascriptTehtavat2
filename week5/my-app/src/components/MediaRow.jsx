93e91effdca767ff30e0b2351b416fa099e85842
Ismet Ymeri <kismet1985@hotmail.fi>
2024-04-14 01:03:01 +0300

    Update MediaRow.jsx

--- week5/my-app/src/components/MediaRow.jsx
+++ week5/my-app/src/components/MediaRow.jsx
@@ -1,1 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
+const MediaRow = ({ item }) => {
+  return (
+    <tr key={item.media_id}>
+      <td>
+        <img src={item.thumbnail} alt={item.title} />
+      </td>
+      <td>{item.title}</td>
+      <td>{item.description}</td>
+      <td>{new Date(item.created_at).toLocaleDateString('fi-FI')}</td>
+      <td>{item.filesize}</td>
+      <td>{item.media_type}</td>
+    </tr>
+  );
+};
+
+MediaRow.propTypes = {
+  item: PropTypes.shape({
+    media_id: PropTypes.number.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    created_at: PropTypes.string.isRequired,
+    filesize: PropTypes.number.isRequired,
+    media_type: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MediaRow;
